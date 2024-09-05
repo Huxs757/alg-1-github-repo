d76868713e927ffbe4c7bd963467ff7658e21983
Ivan <notter59@mail.ru>
2024-09-05 15:26:04 +0300

    Файл создан

---
+++ 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+ 
+using namespace std; 
+ 
+int main() 
+{ 
+    cout << "input x: "; 
+
+    double x;
+    cin >> x;
+
+    double y;
+    if(x>10)
+    {
+        y=-1;
+    }
+    else
+    {
+        y=5;
+    }
+    cout << "y=" << y;
+}
